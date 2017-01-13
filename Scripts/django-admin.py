87729c1cf73372e26cd4d5e2d27d3f83d484d33a
MandaloreLC <test@mail.com>
2017-01-13 11:49:52 +0000

    First commit

---
+++ Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\dev\python_workspace\watchusbuild-teamdirectorywithdjango\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
